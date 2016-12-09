a5d46403b8597580afb4ebfd954f07fb89235673
ali <ali@ali-VIT-P2400>
2016-12-09 12:33:47 -0430

    añadir y modificar, validando en el .zul por constraint

--- zk-test-01/src/main/java/org/test/zk/dialog/Cdialogcontroller.java
+++ zk-test-01/src/main/java/org/test/zk/dialog/Cdialogcontroller.java
@@ -1,6 +1,6 @@
 package org.test.zk.dialog;
 
-
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -24,117 +24,137 @@ import org.zkoss.zul.Selectbox;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Window;
 
-
-
 public class Cdialogcontroller extends SelectorComposer<Component> {
-
     
     private static final long serialVersionUID = 2127738451903407979L;
-    protected Cmanagercontroller mana = new Cmanagercontroller();
-    protected ListModelList< Cperson > dataModel = new ListModelList<Cperson>();
-    protected ListModelList<String> sexo = new ListModelList<String>();//select sexo
-    protected Cperson person;
-    protected Cperson person2;
-    protected Component botonmo;
- 
-    protected ListModelList< Cperson > modelauxi = new ListModelList<Cperson>();
-    protected ListModelList< Cperson > modelauxi2 = new ListModelList<Cperson>();
-        
-        
     
+    protected Cmanagercontroller mana = new Cmanagercontroller();
+    
+    protected ListModelList<Cperson> dataModel = new ListModelList<Cperson>();
+    
+    protected ListModelList<String> sexo = new ListModelList<String>();//select sexo
+    
+    protected Cperson person;
+    protected Component botonmo;
+    protected Component add;
+  
     @Wire
     Window windowpersona;
+    
     @Wire
     Label labelid;
+    
     @Wire
     Textbox textid;
+    
     @Wire
     Label labenombre;
+    
     @Wire
     Textbox textnombre;
+    
     @Wire
     Label labeapellido;
+    
     @Wire
     Textbox textapellido;
+    
     @Wire
     Label labegenero;
+    
     @Wire
     Selectbox selectgenero;
+    
     @Wire
     Label labelfecha;
+    
     @Wire
     Datebox fecha;
+    
     @Wire
     Label labelcomen;
+    
     @Wire
-    Textbox textcomenta; 
-
+    Textbox textcomenta;
+    
     @Override
-    public void doAfterCompose(Component comp) {
-
-            try {
-             
-                super.doAfterCompose(comp);
-                fecha.setFormat( "dd-MM-yyyy" );//arreglamos el error de formato, dandole un formato deseado en tiempo de ejecucion
-                //cargamos al modelo las opciones
-                sexo.add( "Femenino" );//otrocomentario
-                sexo.add( "Masculino" );
-                //se las asignamos a el selec
-                selectgenero.setModel( sexo );
-                //colocamos Femenico como 1a opcion que se muestra para que el selec no se vea vacio
-                selectgenero.setSelectedIndex( 0 );
-                sexo.addToSelection( "Femenino" );
-                
-                
-                final Execution execution = Executions.getCurrent();
-
-                person =(Cperson)execution.getArg().get( "personmodify" );
-                botonmo= (Component) execution.getArg().get("idmodify");
-                
+    public void doAfterCompose( Component comp ) {
+        
+        try {
+            
+            super.doAfterCompose( comp );
+            fecha.setFormat( "yyyy-MM-dd" );//arreglamos el error de formato, dandole un formato deseado en tiempo de ejecucion
+            //cargamos al modelo las opciones
+            sexo.add( "Femenino" );//otrocomentario
+            sexo.add( "Masculino" );
+            //se las asignamos a el selec
+            selectgenero.setModel( sexo );
+            //colocamos Femenico como 1a opcion que se muestra para que el selec no se vea vacio
+            selectgenero.setSelectedIndex( 0 );
+            sexo.addToSelection( "Femenino" );
+            textcomenta.setValue( "inserte comentario" );
+            
+            final Execution execution = Executions.getCurrent();
+            
+            person = ( Cperson ) execution.getArg().get( "personmodify" );
+            botonmo = ( Component ) execution.getArg().get( "idmodify" );
+            add = ( Component ) execution.getArg().get( "agregar" );
+            
+            
+            if(person!=null){
                 textid.setValue( person.getId() );
                 textnombre.setValue( person.getName() );
                 textapellido.setValue( person.getSecondname() );
-                if(person.getGenero() == 0 ){
+                if ( person.getGenero() == 0 ) {
                     sexo.addToSelection( "Femenino" );
-                }else{
+                }
+                else {
                     sexo.addToSelection( "Masculino" );
-                }              
-                fecha.setValue( java.sql.Date.valueOf( person.getNacio() )  );
+                }
+                fecha.setValue( java.sql.Date.valueOf( person.getNacio() ) );
                 textcomenta.setValue( person.getComentario() );//aqui cargamos todo a la vista
-          
-
-                
+                 
             }
-            catch ( Exception e ) {
-              
-                e.printStackTrace();
-            }
-   }
-
-    @Listen("onClick=#idaceptar")
-    public void onClickaceptar (Event event){
-       
-            boolean i=false;
-            person = new Cperson(textid.getValue(), textnombre.getValue(), textapellido.getValue(), 0, null , textcomenta.getValue());
-           while(i==false){
-            if(person.getName()==""){
-                Messagebox.show("algun campo esta en blanco" , "Error", Messagebox.OK, Messagebox.EXCLAMATION );
-                
-            }else{
-                Events.echoEvent( new Event( "onFinaldialog", botonmo, person ) );       
-               windowpersona.detach();//detach se encarga de cerrar la ventana   
-               i=true;
-            }
+               
+        }
+        catch ( Exception e ) {
             
-         }
-       
-    }   
-    @Listen("onClick=#idcancelar")
-    public void onClickcancelar(Event event){
-        //System.out.println("Hello Cancel Des");
-        Messagebox.show("Cancelar", "Canlel", Messagebox.OK, Messagebox.EXCLAMATION);//mmessagebox muestra una ventana emergente con un mensaje
-        windowpersona.detach();
+            e.printStackTrace();
+        }
+    }
     
-    }    
+    @Listen( "onClick=#idaceptar" )
+    public void onClickaceptar( Event event ) {
+                
+              
+               LocalDate nacio = new java.sql.Date(fecha.getValue().getTime()).toLocalDate();
+               
+               //fecha.setValue( java.sql.Date.valueOf( person.getNacio() ) );
+               if(selectgenero.getSelectedIndex() == 1){
+                   
+                   person = new Cperson( textid.getValue(), textnombre.getValue(), textapellido.getValue(), 1,nacio  , textcomenta.getValue() );
+                   
+               }else{
+                   person = new Cperson( textid.getValue(), textnombre.getValue(), textapellido.getValue(), 0, nacio , textcomenta.getValue() );
+               }
+               //person.setNacio( java.sql.Date.valueOf());
+               if(botonmo==null){
+                   Events.echoEvent( new Event( "onAgregar", add, person ) ); 
+               }
+               else{
+                   Events.echoEvent( new Event( "onFinaldialog", botonmo, person ) );    
+               }
+                windowpersona.detach();//detach se encarga de cerrar la ventana   
+    
+    }
+    
+    @Listen( "onClick=#idcancelar" )
+    public void onClickcancelar( Event event ) {
+        
+        //System.out.println("Hello Cancel Des");
+        Messagebox.show( "Cancelar", "Canlel", Messagebox.OK, Messagebox.EXCLAMATION );//mmessagebox muestra una ventana emergente con un mensaje
+        windowpersona.detach();
+        
+    }
     
 }
--- zk-test-01/src/main/java/org/test/zk/manager/Cmanagercontroller.java
+++ zk-test-01/src/main/java/org/test/zk/manager/Cmanagercontroller.java
@@ -30,68 +30,60 @@ import org.zkoss.zul.Window;
 
 import com.lowagie.text.pdf.PushbuttonField;
 
-
 public class Cmanagercontroller extends SelectorComposer<Component> {
-
-   
+    
     private static final long serialVersionUID = 7423111049335041046L;
-   
-    protected ListModelList< Cperson > dataModel = new ListModelList<Cperson>();
-    protected ListModelList<Cperson> aux =  new ListModelList<Cperson>(); 
-    protected int aquiesta;
+    
+    protected ListModelList<Cperson> dataModel = new ListModelList<Cperson>();
+    
+    protected ListModelList<Cperson> aux = new ListModelList<Cperson>();
     
     
-    public class Rendererpersonas implements ListitemRenderer<Cperson>{
-
+    
+    public class Rendererpersonas implements ListitemRenderer<Cperson> {
         
         @Override
         public void render( Listitem lista, Cperson persona, int arg2 ) throws Exception {
             
-            try{
+            try {
                 Listcell id = new Listcell();
                 
                 id.setLabel( persona.getId() );
                 
-                lista.appendChild(id);
-                
+                lista.appendChild( id );
                 
                 Listcell nombre = new Listcell();
                 
                 nombre.setLabel( persona.getName() );
                 
-                lista.appendChild(nombre);
-                
+                lista.appendChild( nombre );
                 
                 Listcell apellido = new Listcell();
                 
-                apellido.setLabel( persona.getSecondname());
+                apellido.setLabel( persona.getSecondname() );
                 
-                lista.appendChild(apellido);
-                
+                lista.appendChild( apellido );
                 
                 Listcell genero = new Listcell();
                 
-                genero.setLabel( persona.getGenero()==0?"female" : "male");
+                genero.setLabel( persona.getGenero() == 0 ? "female" : "male" );
                 
-                lista.appendChild(genero);
-                
-                
+                lista.appendChild( genero );
                 
                 Listcell fecha = new Listcell();
                 
-                fecha.setLabel( persona.getNacio().toString());
+                fecha.setLabel( persona.getNacio().toString() );
                 
-                lista.appendChild(fecha);
-                
+                lista.appendChild( fecha );
                 
                 Listcell comentario = new Listcell();
                 
-                comentario.setLabel( persona.getComentario());
+                comentario.setLabel( persona.getComentario() );
                 
-                lista.appendChild(comentario);
+                lista.appendChild( comentario );
                 
             }
-            catch (Exception ex){
+            catch ( Exception ex ) {
                 ex.printStackTrace();
                 
             }
@@ -99,169 +91,189 @@ public class Cmanagercontroller extends SelectorComposer<Component> {
             // TODO Auto-generated method stub
             
         }
-     }
-
-
+    }
+    
     @Wire
     Listbox listabox;
+    
     @Wire
-    Button idmodify; 
-   
-    
-   
-   
+    Button idmodify;
+    @Wire
+    Button idadd;
     @Override
-    public void doAfterCompose(Component comp) {
-
-            try {
-                super.doAfterCompose(comp);
-                
-              //desde aqui+
-                //creo las personas
-                Cperson persona1= new Cperson("24437593","Ali","Marcano",1, LocalDate.parse( "1995-04-24" ),"lolicon");
-                Cperson persona2= new Cperson("22xxxxxx","German","Cedeño",1, LocalDate.parse( "1994-08-02" ),"kudere a live");
-                Cperson persona3= new Cperson("25xxxxxx","Roger","Paesani",1, LocalDate.parse( "1995-05-24" ),"ui si un comentario");
-                Cperson persona4= new Cperson("25xxxxxx","Juan","Manuel",1, LocalDate.parse( "1995-09-28" ),"nekofilico");
-                //las asigno al ata moder
-                dataModel.add( persona1 );
-                dataModel.add( persona2 );
-                dataModel.add( persona3 );
-                dataModel.add( persona4 );
-                
-             
-                
-              //-hasta aqui si tenemos una "base de datos" seria cuestion se hacer el metodo que selecione uno por 
-              // uno para ir agregando data model  
-              /** 
-               * public void insetdata(Component com, lista listapersona){
-               * if(listapersona!=null){
-               *  
-               *  Cperson loquesea = new Cperson(listapersona.id,listapersona.nombre,listapersona.apellido);
-               *  dataModel.add(loquesea);
-               *  insetdata(comp, lista.sig)
-               *  }
-               * }
-               * 
-               * listbox.setModel(datamodel);
-               * */  
-                
-                //le digo a la lista que agarre el data model
-                
-                
-                dataModel.setMultiple( true ); //por si quieres elecion multiple en la lista
-                listabox.setModel( dataModel );
-                listabox.setItemRenderer( new Rendererpersonas() );
-
+    public void doAfterCompose( Component comp ) {
+        
+        try {
+            super.doAfterCompose( comp );
             
-            }
-            catch ( Exception e ) {
-              
-                e.printStackTrace();
-            }
-
-     }
+            //desde aqui+
+            //creo las personas
+            Cperson persona1 = new Cperson( "24437593", "Ali", "Marcano", 1, LocalDate.parse( "1995-04-24" ), "lolicon" );
+            Cperson persona2 = new Cperson( "22xxxxxx", "German", "Cedeño", 1, LocalDate.parse( "1994-08-02" ), "kudere a live" );
+            Cperson persona3 = new Cperson( "25xxxxxx", "Roger", "Paesani", 1, LocalDate.parse( "1995-05-24" ), "ui si un comentario" );
+            Cperson persona4 = new Cperson( "25xxxxxx", "Juan", "Manuel", 1, LocalDate.parse( "1995-09-28" ), "nekofilico" );
+            //las asigno al ata moder
+            dataModel.add( persona1 );
+            dataModel.add( persona2 );
+            dataModel.add( persona3 );
+            dataModel.add( persona4 );
+            
+            //-hasta aqui si tenemos una "base de datos" seria cuestion se hacer el metodo que selecione uno por 
+            // uno para ir agregando data model  
+            /**
+             * public void insetdata(Component com, lista listapersona){ if(listapersona!=null){
+             * 
+             * Cperson loquesea = new Cperson(listapersona.id,listapersona.nombre,listapersona.apellido); dataModel.add(loquesea); insetdata(comp, lista.sig) } }
+             * 
+             * listbox.setModel(datamodel);
+             */
+            
+            //le digo a la lista que agarre el data model
+            
+            dataModel.setMultiple( true ); //por si quieres elecion multiple en la lista
+            listabox.setModel( dataModel );
+            listabox.setItemRenderer( new Rendererpersonas() );
+            
+        }
+        catch ( Exception e ) {
+            
+            e.printStackTrace();
+        }
+        
+    }
     
-
     //------------------------------------------- inicio añadir---------------------------------------------------------------------
-  
-    @Listen ("onClick=#idadd")
-    public void botonadd(Event event){
-        //en comentarios porque ? 
-        //Map arg = new HashMap();
-        //arg.put("someName", someValue);
-       Window win = ( Window ) Executions.createComponents("/dialog.zul",null , null); //llama a la pagina dialog como una ventana emergente
-       win.doModal();
-    }
-
-    //------------------------------------------- inicio Delete--------------------------------------------------------------------- 
     
-    @SuppressWarnings( { "rawtypes", "unchecked" } )   
-    @Listen ("onClick=#idDelete")
-    public void botondelete(Event event){
-        Set<Cperson> Selecteditem = dataModel.getSelection();
-        
-        if( Selecteditem != null && Selecteditem.size()>0){
-           //esto es para cuando se seleccionaron varias
-           String mostra=null; 
-            
-           for( Cperson person : Selecteditem ){
-             if (mostra != null){
-                 mostra= mostra+"\n"+person.getId()+" "+person.getName()+" "+person.getSecondname();
-             }else{
-                 mostra= person.getId()+" "+person.getName()+" "+person.getSecondname();
-             }
-               
-           }
-           Messagebox.show("Seguro desea eliminar "+ Integer.toString( Selecteditem.size())+" de los registros \n"+mostra , "Eliminar", Messagebox.OK  | Messagebox.CANCEL, Messagebox.QUESTION, new org.zkoss.zk.ui.event.EventListener() {
-               public void onEvent(Event evt) throws InterruptedException {
-                   if (evt.getName().equals("onOK")) {
-                     //eliminar registros
-                      
-                       while( Selecteditem.iterator().hasNext() ){
-                           Cperson person= Selecteditem.iterator().next();
-                           
-                           //Selecteditem.iterator().remove(); 
-                           
-                           dataModel.remove( person );
-                           
-                       }
-
-                   } 
-               }
-           }); 
-           //Messagebox.show( mostra );  
-          //esto es para una persona  
-          // Cperson person=Selecteditem.iterator().next();
-          // Messagebox.show(person.getId()+" "+person.getName()+" "+person.getSecondname());
+    @Listen("onAgregar=#idadd")
+    public void agrega(Event event){
+        Cperson person = ( Cperson ) event.getData();
+        Messagebox.show( person.getComentario() );
+        dataModel.add(person);
  
-        }else{
-            Messagebox.show( "No seleccionaste nada");
-        }
-     
-    }
-    
- //------------------------------------------- inicio modificar---------------------------------------------------------------------------------   
-    
-    @Listen ("onClick=#idmodify")
-    public void botonmodificar(Event event){
-        
-        aux = dataModel;
-        Set<Cperson> Selecteditem = dataModel.getSelection();
-        aquiesta=dataModel.getSelection().size();
-        Messagebox.show( aquiesta+"registro", "Error", Messagebox.OK, Messagebox.EXCLAMATION );
-        
-        
-        if( Selecteditem != null && Selecteditem.size()>0){
-            Cperson person = Selecteditem.iterator().next();
-            
-            Map<String,Object> arg = new HashMap<String,Object>();
-            
-            arg.put("personmodify", person);
-            arg.put( "idmodify", idmodify );
-         
-            
-            Window win = ( Window ) Executions.createComponents("/dialog.zul",null,arg); //llama a la pagina dialog como una ventana emergente
-            win.doModal();
-            
-        }else{
-        
-            Messagebox.show( "No seleccionaste nada");
-        }
-        
-   
-        
-    }
-    
-   //evento que edita el model en la persona y permite volver a renderizar el model 
-    
-    @Listen("onFinaldialog=#idmodify")
-    public void fin(Event evento) {
-        Cperson persona = (Cperson) evento.getData();
-        
-        dataModel.getElementAt( aquiesta ).setId( persona.getId() );
-        dataModel.getElementAt( aquiesta ).setName( persona.getName());
         listabox.setModel( dataModel );
         listabox.setItemRenderer( new Rendererpersonas() );
-      
+        
+    }
+        
+    
+    @Listen( "onClick=#idadd" )
+    public void botonadd( Event event ) {
+        
+        Map<String, Object> arg = new HashMap<String, Object>();
+        
+        arg.put( "agregar", idadd );
+        arg.put( "personmodify", null );
+        arg.put( "idmodify", null );
+        
+        
+        Window win = ( Window ) Executions.createComponents( "/dialog.zul", null, arg ); //llama a la pagina dialog como una ventana emergente
+        win.doModal();
+    }
+    
+    
+    
+    //------------------------------------------- inicio Delete--------------------------------------------------------------------- 
+    
+    @SuppressWarnings( { "rawtypes", "unchecked" } )
+    @Listen( "onClick=#idDelete" )
+    public void botondelete( Event event ) {
+        
+        Set<Cperson> Selecteditem = dataModel.getSelection();
+        
+        if ( Selecteditem != null && Selecteditem.size() > 0 ) {
+            //esto es para cuando se seleccionaron varias
+            String mostra = null;
+            
+            for ( Cperson person : Selecteditem ) {
+                if ( mostra != null ) {
+                    mostra = mostra + "\n" + person.getId() + " " + person.getName() + " " + person.getSecondname();
+                }
+                else {
+                    mostra = person.getId() + " " + person.getName() + " " + person.getSecondname();
+                }
+                
+            }
+            Messagebox.show( "Seguro desea eliminar " + Integer.toString( Selecteditem.size() ) + " de los registros \n" + mostra, "Eliminar", Messagebox.OK | Messagebox.CANCEL, Messagebox.QUESTION, new org.zkoss.zk.ui.event.EventListener() {
+                
+                public void onEvent( Event evt ) throws InterruptedException {
+                    
+                    if ( evt.getName().equals( "onOK" ) ) {
+                        //eliminar registros
+                        
+                        while ( Selecteditem.iterator().hasNext() ) {
+                            Cperson person = Selecteditem.iterator().next();
+                            
+                            //Selecteditem.iterator().remove(); 
+                            
+                            dataModel.remove( person );
+                            
+                        }
+                        
+                    }
+                }
+            } );
+            //Messagebox.show( mostra );  
+            //esto es para una persona  
+            // Cperson person=Selecteditem.iterator().next();
+            // Messagebox.show(person.getId()+" "+person.getName()+" "+person.getSecondname());
+            
+        }
+        else {
+            Messagebox.show( "No seleccionaste nada" );
+        }
+        
+    }
+    
+    //------------------------------------------- inicio modificar---------------------------------------------------------------------------------   
+    
+    @Listen( "onClick=#idmodify" )
+    public void botonmodificar( Event event ) {
+        
+        
+        Set<Cperson> Selecteditem = dataModel.getSelection();
+     
+        if ( Selecteditem != null && Selecteditem.size() > 0 ) {
+            Cperson person = Selecteditem.iterator().next();
+            
+            Map<String, Object> arg = new HashMap<String, Object>();
+            
+            arg.put( "personmodify", person );
+            arg.put( "idmodify", idmodify );
+            
+            Window win = ( Window ) Executions.createComponents( "/dialog.zul", null, arg ); //llama a la pagina dialog como una ventana emergente
+            win.doModal();
+            
+        }
+        else {
+            
+            Messagebox.show( "No seleccionaste nada" );
+        }
+        
+    }
+    
+    //evento que edita el model en la persona y permite volver a renderizar el model 
+    
+    @Listen( "onFinaldialog=#idmodify" )
+    public void fin( Event evento ) {
+        
+        Cperson persona = ( Cperson ) evento.getData();
+        int i=0;
+        Messagebox.show( dataModel.getSize()+1+"numero" );
+        while(i<=dataModel.getSize()){
+            if(dataModel.getElementAt(i).getId() == persona.getId()){
+                dataModel.getElementAt(i ).setId( persona.getId() );
+                dataModel.getElementAt( i ).setName( persona.getName() );
+                dataModel.getElementAt(i).setSecondname( persona.getSecondname() );
+                dataModel.getElementAt( i ).setGenero( persona.getGenero() );
+                dataModel.getElementAt( i ).setNacio( persona.getNacio() );
+                dataModel.getElementAt( i ).setComentario( persona.getComentario() );
+            i=dataModel.getSize();
+            }
+            i=i+1;
+
+        }
+        listabox.setModel( dataModel );
+        listabox.setItemRenderer( new Rendererpersonas() );
+        
     }
 }
